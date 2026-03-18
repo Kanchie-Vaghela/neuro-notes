10046fe108ce3b0bcf1d04c7992aeaa37f363c43
kanchi <KanchiVaghela.ict23@adaniuni.ac.in>
2026-03-18 22:36:54 +0530

    Implement auth middleware

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
 import authRoutes from "./routes/auth.routes.js"
+import { protect } from "./middleware/auth.middleware.js"
 
 dotenv.config()
 
@@ -15,6 +16,9 @@ app.use(express.json())
 connectDB()
 
 app.use("/auth", authRoutes)
+app.get("/test", protect, (req, res) => {
+  res.json({ userId: req.user })
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
